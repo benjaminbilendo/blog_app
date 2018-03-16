b6edfe0a5f911019e99609d083c69c32719c56c7
Benjamin Sunderland <ben@sunderland.eu.com>
2018-03-16 18:14:54 +0100

    Recommit

--- app/controllers/backend/subscriptions_controller.rb
+++ app/controllers/backend/subscriptions_controller.rb
@@ -1,5 +1,6 @@
 module Backend
     class SubscriptionsController < ApplicationController
+        before_action :find_subscription
         def create
             @subscription = Subscription.new(subscription_params)
             if @subscription.save
@@ -20,4 +21,43 @@ module Backend
             params.require(:subscription).permit(:first_name, :last_name, :email)
         end
     end
+end
+
+
+
+
+before_action :find_blog, only: [:show, :edit, :update, :destroy, :active]
+
+def index
+    @blogs = Blog.all.paginate(page: params[:page])
+end
+
+def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+        redirect_to backend_blogs_path, notice: "Success!"
+    else
+        render "new"
+    end
+end
+
+def edit
+end
+
+def new
+    @blog = Blog.new
+end
+
+def update
+    if @blog.update(blog_params)
+      redirect_to backend_blogs_path
+    else
+      render 'edit'
+    end
+end
+
+def destroy
+    @blog.destroy
+    flash[:notice] = "Delete success"
+    redirect_to backend_blogs_path
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,6 @@ class UserMailer < ApplicationMailer
     def new_post(blog_id, subscription_id)
         @blog = Blog.find(blog_id)
         @subscription = Subscription.find(subscription_id)
-
         mail(to: @subscription.email, subject: "See my new post: #{@blog.title}")
     end
 end
